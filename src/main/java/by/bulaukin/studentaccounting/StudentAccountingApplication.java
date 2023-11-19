d270f2396ddbbe60d548b7c788c20cd592337977
Sensationbel <dbulavkin@gmail.com>
2023-11-19 12:06:42 +0100

    create app

---
+++ src/main/java/by/bulaukin/studentaccounting/StudentAccountingApplication.java
@@ -0,0 +1,13 @@
+package by.bulaukin.studentaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAccountingApplication.class, args);
+	}
+
+}
